aaf7c42fbac3557fa52ea972570554cd154fbeaa
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-12-29 04:32:42 +0600

    Implemented order detail view for patch and delete requests.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, OrderListView
+from .views import ProductListView, ProductDetailView, OrderListView, OrderDetailView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 
-    path('orders/', OrderListView.as_view(), name='order-list')
+    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('orders/<str:id>/', OrderDetailView.as_view(), name='order-detail')
 ]
--- api/views.py
+++ api/views.py
@@ -3,9 +3,12 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 from .models import Product, Order, OrderItem
 from .serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+
+User = get_user_model()
 
 class ProductListView(APIView):
     permission_classes = [AllowAny]
@@ -57,14 +60,28 @@ class OrderListView(APIView):
         serializer = OrderSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response({"success": True, "orders": serializer.data, "message": "Order created successfully!"}, status=HTTP_201_CREATED)
         return Response({"success": False, "orders": serializer.data, "errors": serializer.errors}, status=HTTP_400_BAD_REQUEST)
 
 class OrderDetailView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request, pk):
-        order = get_object_or_404(Order, pk=pk)
+    def get(self, request, id):
+        order = get_object_or_404(Order, order_id=id)
         serializer=OrderSerializer(order)
         return Response({"success": True, 'order': serializer.data}, status=HTTP_200_OK)
+    
+    def patch(self, request, id):
+        order = get_object_or_404(Order, order_id=id)
+        serializer = OrderSerializer(order, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": True, "order": serializer.data, "message": "Order updated successfully!"}, status=HTTP_200_OK)
+        return Response({"success": False, "order": serializer.data}, status=HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        order = get_object_or_404(Order, order_id=id)
+        order.delete()
+        return Response({"success": True, "message": "Order deleted successfully!"}, status=HTTP_204_NO_CONTENT)
